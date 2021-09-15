acbb7fdf372f2b6f3a497f2664954011ddcfed50
Mel Vint <melvint@Mels-MacBook-Pro.local>
2021-09-14 21:22:01 -0400

    testing

--- index.js
+++ index.js
@@ -1,30 +1,137 @@
-.then(answers => {
-    const intern = Intern(answers.internName, answers.intern)
-    teamMembers.push(intern);
-    idArray.push(answers.internId);
-    });
+const Employee = require("./Lib/Employee");
+const Employee = Manager("./Lib/Manager");
+const Employee = Intern("./Lib/Intern");
+const Employee = Engineer("./Lib/Engineer");
+const fs = require("fs");
+const inquire = require("inquirer");
 
-]).then(answers => {
-    const Engineer = Engineer(answers.github,answers.getGithub)
-    teamMembers.push(Engineer);
-    idArray.push(answers.engineerId);
-    });
-
-]).then(answers => {
-    const Intern = Intern(answers.internName, answers.internName)
-    teamMembers.push(intern);
-    idArray.push(answers.internId);
+const teamMembers = [];
+ managerPrompt();
+// TODO: Create an array of questions for user input
+function managerPrompts() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "managerName",
+            message: "What is the team managers name?"
+        },
+        {
+            type: "input",
+            name: "managerId",
+            message: "What is the managers ID number?"
+        },
+        {
+            type: "input",
+            name: "managerEmail",
+            message: "What is the managers email?"
+        },
+        {
+            type: "input",
+            name: "managerOffice",
+            message: "What is the managers office room number?"
+        }
+    ]).then(function(response) {
+        const manager = new Manager(response.managerName, response.managerId, response.managerEmail, response.managerOffice)
+        teamMembers.push(manager)
+        newMember();
     });
 }
+
+function newMember() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "position",
+            message: "What position would you like to add to team?",
+            choices: ["Intern", "Engineer", "Hiring Completed"]
+        }
+    ]).then(function(response){
+        if(response.position === "Intern"){
+            internPrompts();
+        } else if(response.position === "Engineer") {
+            engineerPrompts();
+        } else {
+            generateMarkdown();
+            
+        }
+    });
+}
+
+function engineerPrompts() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "engineerName",
+            message: "What is the engineers name?"
+        },
+        {
+            type: "input",
+            name: "engineerId",
+            message: "What is the engineers ID number?"
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "What is the engineers email address?"
+        },
+        {
+            type: "input",
+            name: "gitHub",
+            message: "What is the engineers GitHub username?"
+        }
+    ]).then(function(response) {
+        const engineer = new Engineer(response.engineerName, response.engineerId, response.engineerEmail, response.gitHub);
+        teamMembers.push(engineer);
+        newMember();
+    });
+}
+
+function internPrompts (){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "internName",
+            message: "What is the interns name?"
+        },
+        {
+            type: "input",
+            name: "internId",
+            message: "What is the interns ID number?"
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "What is the interns email address?"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What is the interns school?"
+        }
+    ]).then(function(response) {
+        const intern = new Intern(response.internName, response.internId, response.internEmail, response.school);
+        teamMembers.push(intern);
+        newMember();
+    });
+}
+
+then(answers => {
+    const Intern = Intern(answers.internName, answers.internName);
+    teamMembers.push(intern);
+    idArray.push(answers.internId);
+    createReadStream();
+     });
     
     
-    
-    function BuildingTeam() {
+     function BuildingTeam() {
     if(!fs.existsSync(OUTPUT_DIR)) {
         fs.mkdir(OUTPUT_DIR)
+     }
+     fs.writeFileSync(OutputPath, render(temaMembers), "utf-8");
     }
-    fs.writeFileSync(OutputPath, render(temaMembers), "utf-8");
-}
+
 createManager();
+
+
 
 appMenu();
